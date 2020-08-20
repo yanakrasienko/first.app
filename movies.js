06236bd4000480a4cc5b77c49d085fb019432e84
yanakrasienko <ykrasienko@gmail.com>
2020-08-20 10:52:16 +0300

    functions added

--- movies.js
+++ movies.js
@@ -10,11 +10,30 @@ const personalMovieDb={
 };
 console.log(personalMovieDb["count"]);
 
-const a=prompt('Один из последних просмотренных фильмов?',''),
-  b=prompt('Рейтинг?','') ,
-  c=prompt('Один из последних просмотренных фильмов?',''),
- f=prompt('Рейтинг?','') ;
-  
-   personalMovieDb.movies[a]=b;
-   personalMovieDb.movies[c]=f;
+for (let i=0; i<2;i++){
+  const a=prompt('Один из последних просмотренных фильмов?',''),
+  b=prompt('Рейтинг?','');
+ 
+  if(a!=null && b!=null && a!='' && b!='' && a.length<50){
+    personalMovieDb.movies[a]=b;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+  if(personalMovieDb.count<10){
+    console.log('мало');
+  }else if(personalMovieDb.count>=10 && personalMovieDb.count<30){
+    console.log('классный зритель');
+  }else if(personalMovieDb.count>=30){
+    console.log('вы киноман');
+  }else{
+    console.log('error');
+  }
+}
+
+
+
    console.log(personalMovieDb);
+  
+  
